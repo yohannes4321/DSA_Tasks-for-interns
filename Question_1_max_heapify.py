312d0ce9a895fa78e370d41c5a6953819f96567f
yohannes4321 <alemuyohannes960@gmail.com>
2024-11-04 19:56:04 +0300

    max heapify

--- Question_1_max_heapify.py
+++ Question_1_max_heapify.py
@@ -1,18 +1,24 @@
-def Max_heapify(Array, current_index):
+def max_heapify(Array, current_index):
     largest = current_index  # initialize the largest with current index
     while True:
         left = 2 * current_index + 1  # this is the left child index
         right = 2 * current_index + 2  # this is the right child index
         if left < len(Array) and Array[left] > Array[largest]:
-            # check if the left index is within bounds and if left child is greater than the largest
+            # to cheak if the left index is less than the size of Array so it will stop at the end of the leaf index
+            # the value of the left child is greater than the value of the largest index 
+            # we gona assign the largest with left index
             largest = left
         if right < len(Array) and Array[right] > Array[largest]:
             # check if the right index is within bounds and if right child is greater than the largest
             largest = right
         if largest != current_index:
-            # swap the current index with the largest child
+            # to cheak if the right index is less than the size of Array so it will stop at the end of the right index
+            # the value of the right  child is greater than the value of the largest index 
+            # we gona assign the largest with right  index
+
             Array[current_index], Array[largest] = Array[largest], Array[current_index]
             # move down the tree to the largest child index
             current_index = largest
         else:
-            break  # terminate when current_index is the same as largest
+            break 
+            # terminate when current_index is same as largest
