2824f803826b34528513356b41afc4b617e60b7f
yohannes4321 <alemuyohannes960@gmail.com>
2024-11-04 20:12:07 +0300

    heap sort

---
+++ Question3_Read_About_heap_sort.py
@@ -0,0 +1,12 @@
+"""what i read about HeapSort
+
+time complexity is O(nlogn)
+it use heap data structure and inplace
+
+steps 
+    1,Build heap sort which is heapifying  Array calling 
+    Commonly max heapify on the root node from the last None Leaf node
+    2. bY Swapping the first root index with the last element delete the last element
+    which means decrease the size of Array for heapify method 
+    
+"""
